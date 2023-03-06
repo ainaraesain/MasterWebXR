ea0fd8425df94957b4327674dc1bb7714d3714ec
ainaraesain <78446672+ainaraesain@users.noreply.github.com>
2023-03-06 20:02:27 +0100

    cambios en main.js

--- WebXR_vr_dragging/js/main.js
+++ WebXR_vr_dragging/js/main.js
@@ -153,6 +153,7 @@ function onSelectEnd( event ) {
         const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
         const cube = new THREE.Mesh( geometry, material );
         scene.add( cube );
+        contador = 0;
     }
 
 }
